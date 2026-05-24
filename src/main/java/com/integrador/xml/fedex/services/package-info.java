bca56c800cf2a52d4e68274733ed0416fcd5356b
Jesus Silva <jesussilva@MacBook-Air-de-Jesus.local>
2026-05-24 13:08:58 -0600

    EstafetaRestV2 Token Implementation

--- src/main/java/com/integrador/xml/fedex/services/package-info.java
+++ src/main/java/com/integrador/xml/fedex/services/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2026.05.08 a las 09:29:37 PM CST 
+// Generado el: 2026.05.24 a las 01:55:36 PM CDT 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.integrador.com/xml/fedex/services", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
