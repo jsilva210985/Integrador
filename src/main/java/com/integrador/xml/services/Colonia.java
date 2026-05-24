2d2e0d93800666a53399a51758973f579e128956
Jesus Silva <jesussilva@MacBook-Air-de-Jesus.local>
2026-05-24 12:27:25 -0600

    Last changes Fedex

--- src/main/java/com/integrador/xml/services/Colonia.java
+++ src/main/java/com/integrador/xml/services/Colonia.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2026.04.10 a las 11:29:11 AM CDT 
+// Generado el: 2026.05.08 a las 09:29:37 PM CST 
 //
 
 
